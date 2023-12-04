f90863b087e3800a992ae1fd7890a830b69d2f84
ribeirober1208 <ribeirober1208@gmail.com>
2023-12-04 19:21:58 -0300

    Add Assembler Manager

---
+++ Assets/AssemblerManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class AssemblerManager : MonoBehaviour
+{
+    private GameObject lastObject = null;
+    public List<GameObject> sequence;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
